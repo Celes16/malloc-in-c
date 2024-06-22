2888debe5c08e9dca96f1bab7bdc7dbb3076acf4
Maria Celeste <155506763+Celes16@users.noreply.github.com>
2024-06-21 23:50:23 -0300

    Update malloc.c

--- malloc.c
+++ malloc.c
@@ -2,41 +2,67 @@
 #include <stdlib.h>
 
 struct cel {
-   int        conteudo;
+   int conteudo;
    struct cel *prox;
 };
 
 typedef struct cel cel;
 
-//função principal argc = argumentos e argv = argumentos 
 int main(int argc, char const *argv[]) {
    FILE *arquivo;
-   cel  *lista, *nova, *posicao, *p;
-   int  v;
+   cel *lista, *nova, *posicao, *p;
+   int v;
 
    arquivo = fopen("entrada.txt", "r");
-
-   lista       = (cel *)malloc(sizeof(struct cel));
+   if (arquivo == NULL) {
+      perror("Error opening file");
+      return EXIT_FAILURE;
+   }
    
-   // cabeca da lista sem conteudo, para criar uma lista vazia sem cabeca basta colocar celula *ini; ini = NULL;//
+   lista = (cel *)malloc(sizeof(struct cel));
+   if (lista == NULL) {
+      perror("Error allocating memory");
+      fclose(arquivo);
+      return EXIT_FAILURE;
+   }
+
    lista->prox = NULL;  
-           posicao->prox != NULL && posicao->prox->conteudo > v;
-           posicao = posicao->prox); // p->prox o endereco do proximo elemento//
 
-      nova           = (cel *)malloc(sizeof(struct cel));
+   while (fscanf(arquivo, "%d", &v) != EOF) {
+      posicao = lista;
+
+      while (posicao->prox != NULL && posicao->prox->conteudo > v) {
+         posicao = posicao->prox;
+      }
+
+      nova = (cel *)malloc(sizeof(struct cel));
+      if (nova == NULL) {
+         perror("Error allocating memory");
+         fclose(arquivo);
+         return EXIT_FAILURE;
+      }
+
       nova->conteudo = v;
-
-      nova->prox    = posicao->prox;
+      nova->prox = posicao->prox;
       posicao->prox = nova;
    }
    fclose(arquivo);
-
+   
    arquivo = fopen("saida.txt", "w");
-
-   for (p = lista->prox; p != NULL; p = p->prox)
+   if (arquivo == NULL) {
+      perror("Error opening file");
+      return EXIT_FAILURE;
+   }
+   for (p = lista->prox; p != NULL; p = p->prox) {
       fprintf(arquivo, "%d\n", p->conteudo);
-
+   }
    fclose(arquivo);
+   
+   while (lista != NULL) {
+      p = lista;
+      lista = lista->prox;
+      free(p);
+   }
 
    return 0;
 }
